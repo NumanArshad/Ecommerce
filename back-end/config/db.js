edb3ca7d37c9373895504608aab8f4b2ff60e2a9
usmankhandev <osmankhan.dev@gmail.com>
2020-08-20 02:51:30 +0500

    Db connection setting.

---
+++ back-end/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const connectionstr = "mongodb://localhost:27017/khareedlodb";
+
+const options = {
+    reconnectTries: Number.MAX_VALUE,
+    pooleSize: 10
+}
+
+mongoose.connect (connectionstr, options).then(
+    () => {
+        console.log("Database Connection Established.")
+    },
+    err => {
+        console.log("Error connecting to database.", err)
+    }
+)
---
+++ back-end/server.js
@@ -0,0 +1,16 @@
+const config = require('./config/db')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const port = process.env.PORT || 3000
+const app = express()
+
+app.use(cors())
+app.options('*', cors())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+
+
+app.listen(port, () => {
+    console.log(`Server is running at port ${port}`)
+}) 
